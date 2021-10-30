417496b60a327bb604b28ef2e83ff0eeb1ce7f4d
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-10-30 12:09:21 -0500

    a basic component was created using React. These files will be removed to use an entire development environment

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+// start
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+
 try {
     const root = document.querySelector('#root')
-    root.textContent = "Hello, world!"
+    // root.textContent = "Hello, world!"
+    const domContainer = document.querySelector('#root');
+    ReactDOM.render(e(LikeButton), domContainer);
     
 } catch (error) {
     console.log("Oops!");
 }
 
 
+// end
