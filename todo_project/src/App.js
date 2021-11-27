56901f77e0d78c9bc5dd1d1072c7c159c3227837
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-27 08:01:36 -0500

    project was cleaned up

--- todo_project/src/App.js
+++ todo_project/src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App({message, children}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p>
-        <p>
-          {children}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
