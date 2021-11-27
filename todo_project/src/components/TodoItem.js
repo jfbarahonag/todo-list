0568babd357191b716528bd6c2f79ad8334afb15
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-27 14:32:45 -0500

    TodoItem was modfiied

--- todo_project/src/components/TodoItem.js
+++ todo_project/src/components/TodoItem.js
@@ -4,29 +4,27 @@ import './TodoItem.css'
 function TodoItem({ completed, text }) {
     return (
       <li className="todo-item__card">
-        {completed ? (
-          <svg
-            className="todo-item__card-check"
-            style={{ width: "24px", height: "24px" }}
-            viewBox="0 0 24 24"
-          >
+        <svg
+          className={
+            completed
+            ? "todo-item__card-check"
+            : "todo-item__card-not-check"
+          }
+          style={{ width: "24px", height: "24px" }}
+          viewBox="0 0 24 24"
+        >
+          {completed ? (
             <path
               fill="currentColor"
               d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"
             />
-          </svg>
-        ) : (
-          <svg
-            className="todo-item__card-not-check"
-            style={{ width: "24px", height: "24px" }}
-            viewBox="0 0 24 24"
-          >
+          ) : (
             <path
               fill="currentColor"
               d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
             />
-          </svg>
-        )}
+          )}
+        </svg>
         <span
           className={
             completed
