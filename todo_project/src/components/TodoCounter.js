e83af73dbf03adbc61cde3716b01c59a37d8c9ca
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-27 08:50:55 -0500

    CSS styles were added to TodoCounter component

--- todo_project/src/components/TodoCounter.js
+++ todo_project/src/components/TodoCounter.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './TodoCounter.css'
+
+const styles = {
+    color: 'red',
+    backgroundColor: 'green'
+}
 
 function TodoCounter() {
     return (
-        <h2>You have completed 2 of 3 TODOs</h2>
+        <h2 style={ styles }
+        >
+            You have completed 2 of 3 TODOs
+        </h2>
     )
 }
 
